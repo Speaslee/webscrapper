38dafb392c3e8162b397d8dd070f71f7fa1ae75c
Speaslee <sophiapeaslee@gmail.com>
2015-10-14 17:58:09 -0400

    stuff

--- lib/page.rb
+++ lib/page.rb
@@ -1,36 +1,50 @@
 require 'mechanize'
 
 class Page < ActiveRecord::Base
-validates_presence_of :name, :url
-validates_uniqueness_of :url
-has_many :links
+  validates_presence_of :name, :url
+  validates_uniqueness_of :url
+  has_many :links
+  attr_reader :list_o_links
+  def self.save_random url = nil
+    #search for random page
 
-def self.save_random
-#search for random page
-agent = Mechanize.new
-page = agent.get("http://wikipedia.org/wiki/Main_Page")
-link = page.link_with(text: 'Random article')
-page = link.click
-# 
-# body_text= page.search("body p")
-# results = []
-# paragraphs = body_text.each do |para|
-#  something = para.search "p"
-#   results.push(something)
-#   end
+    agent = Mechanize.new
+    if url
+      page = agent.get(url)
+    else
+      page = agent.get("http://wikipedia.org/wiki/Main_Page")
+    end
+    link = page.link_with(text: 'Random article')
+    page = link.click
+    @list_o_links = (page.search("p").search("a")).to_a
+    #
+    # body_text= page.search("body p")
+    # results = []
+    # paragraphs = body_text.each do |para|
+    #  something = para.search "p"
+    #   results.push(something)
+    #   end
 
-i = self.new(
-name: page.title,
-prelude: page.search("p").first,
-url: page.uri
-)
-i.save
+    i = self.new(
+    name: page.title,
+    prelude: page.search("p").first,
+    url: page.uri
+    )
+    i.save
 
-end
+  end
 
-def follow_link (nums)
+  def follow_link nums
 
-end
+    list_o_url= @list_o_links.map {|l| "http://www.wikipedia.org"+l.atributes["href"].value}
+
+
+    list_o_url.sample(nums).each do |u|
+      save_page url
+      binding.pry
+    end
+
+  end
 
 
 
